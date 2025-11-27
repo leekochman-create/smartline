ed237c3837cfd7b5d9e70ffa0dd9224322208cdf
leekochman-create <leekochman@gmail.com>
2025-11-27 15:57:11 +0200

    Update route.ts

--- app/api/camera/route.ts
+++ app/api/camera/route.ts
@@ -4,8 +4,8 @@ export async function POST(req) {
   const { url } = await req.json();
 
   try {
-    const img = await fetch(url);
-    const buffer = await img.arrayBuffer();
+    const response = await fetch(url);
+    const buffer = await response.arrayBuffer();
     const base64 = Buffer.from(buffer).toString("base64");
 
     return NextResponse.json({ image: base64 });
