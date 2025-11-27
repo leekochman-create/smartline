1f93a398b7040c8b8c23d54d5a329ba5b886222d
leekochman-create <leekochman@gmail.com>
2025-11-27 15:30:23 +0200

    Create camera.ts

---
+++ lib/camera.ts
@@ -0,0 +1,8 @@
+export async function fetchCameraFrame(url: string) {
+  const res = await fetch("/api/camera", {
+    method: "POST",
+    body: JSON.stringify({ url })
+  });
+
+  return await res.json();
+}
