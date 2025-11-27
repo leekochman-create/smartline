8162c95b79608519ca0cdb51f47b331d629ff753
leekochman-create <leekochman@gmail.com>
2025-11-27 15:34:57 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="he">
-      <body>{children}</body>
+      <body>
+
+        {/* Google Maps API */}
+        <script
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}&libraries=visualization`}
+          async
+        ></script>
+
+        {children}
+      </body>
     </html>
   );
 }
