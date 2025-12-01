ba326e00163f08d3db60fa5f406adcf525a9488f
leekochman-create <leekochman@gmail.com>
2025-12-01 17:01:29 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import GlobalMap from "./components/GlobalMap";
+// smartline/app/page.tsx
+"use client";
+
+import IsraelCamerasMap from "./components/IsraelCamerasMap";
 
 export default function HomePage() {
   return (
-    <div style={{ width: "100%", height: "100vh" }}>
-      <GlobalMap />
+    <div style={{ padding: 20 }}>
+      <h1 style={{ textAlign: "center", marginBottom: 20 }}>
+        מפת מצלמות ישראל (לייב)
+      </h1>
+
+      <IsraelCamerasMap />
     </div>
   );
 }
